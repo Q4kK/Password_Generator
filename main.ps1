0d246435be05fe048649125521d4eadb3fe4f576
Q4kK <quantumstar4k@gmail.com>
2023-03-20 20:04:38 -0500

    Feat: Added support for special characters

--- main.ps1
+++ main.ps1
@@ -1,13 +1,15 @@
 $Alphabet_Lowercase = ("a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 $Alphabet_Uppercase = ("A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z")
 $Numeric_Values = (0..9)
+$Special_Characters = ("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "/", "-", "|", "\")
 
     $Full_Alphabet += $Alphabet_Uppercase
     $Full_Alphabet +=$Alphabet_Lowercase
     $Full_Alphabet += $Numeric_Values
+    $Full_Alphabet += $Special_Characters
 
 for ($i=0; $i -le 10; $i++) {
-    $Random = Get-Random -Maximum 58
+    $Random = Get-Random -Maximum ($Full_Alphabet.Count)
     $Generated_Password += $Full_Alphabet[$Random]
 }
 
