4375595db34c8738f28501bcbc5314c8844483ea
Q4kK <quantumstar4k@gmail.com>
2023-03-27 11:08:42 -0500

    Feat: Added user prompts for all pw components
    User is now prompted with the choice to include uppercase, lowercase,
    numbers, and special characters all in the beginning.

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ special_characters = "!@#%^&*()~/-|"
 full_alphabet = []
 generated_password = ""
 
-full_alphabet.extend(alphabet_uppercase)
-full_alphabet.extend(alphabet_lowercase)
-full_alphabet.extend(numeric_values)
+if input("Do you want uppercase letters? [Y][N]: ").lower() == "y":
+    full_alphabet.extend(alphabet_uppercase)
+if input("Do you want lowercase letters? [Y][N]: ").lower() == "y":
+    full_alphabet.extend(alphabet_lowercase)
+if input("Do you want numbers? [Y][N]: ").lower() == "y":
+    full_alphabet.extend(numeric_values)
+if input("Do you want special characters? [Y][N]: ").lower() == "y":
+    full_alphabet.extend(special_characters)
 
 pw_num = int(input("Enter the amount of passwords you want to generate:"))
 try:
@@ -18,12 +23,8 @@ try:
 except:
     pass_chars = 11
 
-if input("Do you want special characters? [Y][N]:").lower() == "y":
-    full_alphabet.extend(special_characters)
-
 for c in range(pw_num):
     for i in range(pass_chars):
         generated_password += random.choice(full_alphabet)
     print(generated_password)
     generated_password = ""
-
